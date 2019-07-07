2306bb6c90ea6a1008ce78ee306174eaba4699bb
ginpajac <ginpajac@espol.edu.ec>
2019-07-07 00:43:06 -0500

    3 files modified, 1 deleted

--- index.js
+++ index.js
@@ -1,25 +1,67 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 import { render } from 'react-dom';
 import Hello from './Hello';
 import taller from ".taller";
 import './style.css';
+function asignar(){
+
+}
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'Ginger'
+  constructor(props) {
+    super(props);
     
-      
+    this.state = {
+      error: null,
+      isLoaded: false,
+      escritores: [],
+      frases: []
     };
   }
 
+  componentDidMount() {
+    fetch("https://dataserverdaw.herokuapp.com/escritores")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            escritores: result.escritores
+          });
+        },
+
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  
   render() {
-    return (
-      <div>
-        <taller />
-      </div>
-    );
+    const { error, isLoaded,escritores } = this.state;
+    var cnt=0;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul class='nav nav-tabs' role="tablist">
+          {escritores.map(f => (
+            <li  class='nav-item'>
+              <a class='nav-link' data-toggle="tab" role="tab">
+                {f.nombre}
+              </a> 
+            </li>
+          ))}
+        </ul>
+      );
+    }
   }
 }
 
