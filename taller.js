ca4b3ad022d265a200b348d4cc830726644ce591
ginpajac <ginpajac@espol.edu.ec>
2019-07-07 00:09:26 -0500

    1 file modified

--- taller.js
+++ taller.js
@@ -1,10 +1,53 @@
 import React from 'react';
 
-const taller =() =>{
-  return (
-    <div>
-      <h2>GINGER ESTUVO AQUI</h2>
-    </div>
-  )
+class frases extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://dataserverdaw.herokuapp.com/escritores/frases")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.items
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <li key={item.name}>
+              {item.name} {item.price}
+            </li>
+          ))}
+        </ul>
+      );
+    }
+  }
 }
-export default taller;
